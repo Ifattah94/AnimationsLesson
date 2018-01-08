8783f16fb3bf9d87cd8dd4445f08f034604eb05b
Iram Fattah <iramfattah94@gmail.com>
2018-01-08 12:39:57 -0500

    diferent animations

--- animations/ViewController.swift
+++ animations/ViewController.swift
@@ -10,16 +10,70 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var originalPosition: CGRect = .zero
+    var originalAngle: CGFloat = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        originalPosition = squareView.frame
+        
+    }
+    
+
+    @IBOutlet weak var squareView: UIView!
+    
+    @IBOutlet weak var animateButton: UIButton!
+    
+    func rotate() {
+         let newAngle = self.originalAngle + CGFloat.pi
+        
+        UIView.animate(withDuration: 3.0) {
+            self.squareView.transform = CGAffineTransform.init(rotationAngle: newAngle)//rotates the view
+            self.originalAngle = newAngle
+        }
+    }
+    
+    func reiazjazz() { //changing the view color and size
+        var finalPosition = CGRect()
+        if squareView.frame == originalPosition {
+            finalPosition = view.safeAreaLayoutGuide.layoutFrame
+        } else {
+            finalPosition = originalPosition
+        }
+        
+        UIView.animate(withDuration: 2.0, animations: {
+                //let newAngle = self.originalAngle + CGFloat.pi
+                self.squareView.frame = finalPosition
+                
+                if self.squareView.backgroundColor != .orange {
+                    self.squareView.backgroundColor = .orange
+                    self.squareView.frame = finalPosition
+                    
+                }
+                else {
+                    self.squareView.backgroundColor = .cyan
+                    self.squareView.frame = self.originalPosition
+                }
+        })
+    }
+    
+    
+    @IBAction func animateButtonPressed(_ sender: UIButton) {
+        animateButton.isEnabled = false
+        
+            //completion handler below runs after animation
+            reiazjazz()
+//            rotate()
+            self.animateButton.isEnabled = true
+            print("completion success")
+        }
+        
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
-}
 
